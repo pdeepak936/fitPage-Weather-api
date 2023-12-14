ff9665c574311e900fb85d81972589bb6c97665d
pdeepak936 <67180496+pdeepak936@users.noreply.github.com>
2023-12-14 20:59:39 +0530

    weather history update

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -3,16 +3,32 @@ const {
   getWeatherDataHistory,
 } = require("../services/externalApiService.js");
 const LocationModel = require("../models/locationModel.js");
+const WeatherDataModel = require("../models/weatherDataModel.js")
 
 exports.getWeatherByLocationId = async (req, res) => {
   const { locationId } = req.params;
+
   try {
-    const data = await LocationModel.findById(locationId);
-    const lat = data.latitude;
-    const lon = data.longitude;
+    const location = await LocationModel.findById(locationId);
+    if (!location) {
+      return res.status(404).json({ error: 'Location not found' });
+    }
+
+    const lat = location.latitude;
+    const lon = location.longitude;
+
     const weatherData = await getWeatherData(lat, lon);
-    res.json(weatherData);
+
+    // Save the weather data into a new collection or update an existing collection
+    const savedWeatherData = await WeatherDataModel.create({
+      locationId: locationId,
+      data: weatherData,
+      timestamp: new Date(),
+    });
+
+    res.json(savedWeatherData);
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
@@ -24,23 +40,32 @@ function getUnixTimestamp(daysAgo) {
 }
 
 exports.weatherHistory = async (req, res) => {
-  const { days, id } = req.query;
+  const { days, id } = req.body;
 
   const start = getUnixTimestamp(days);
   const end = getUnixTimestamp(0);
 
   try {
-    const data = await LocationModel.findById(id);
-    const lat = data.latitude;
-    const lon = data.longitude;
-    const weatherDataHistory = await getWeatherDataHistory(
-      lat,
-      lon,
-      start,
-      end
-    );
-    res.json(weatherDataHistory);
+    const location = await LocationModel.findById(id);
+    if (!location) {
+      return res.status(404).json({ error: 'Location not found' });
+    }
+
+    const lat = location.latitude;
+    const lon = location.longitude;
+
+    const weatherDataHistory = await getWeatherDataHistory(lat, lon, start, end);
+
+    // Save the historical weather data into a new collection
+    const savedWeatherHistory = await WeatherHistoryModel.create({
+      locationId: id,
+      data: weatherDataHistory,
+      timestamp: new Date(),
+    });
+
+    res.json(savedWeatherHistory);
   } catch (error) {
-    res.status(500).json({ error: "Internal Server Error",error });
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error", error });
   }
 };
---
+++ models/weatherDataModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const weatherDataSchema = new mongoose.Schema({
+  locationId: { type: mongoose.Schema.Types.ObjectId, ref: 'Location', required: true },
+  data: { type: Object, required: true },
+  timestamp: { type: Date, required: true },
+});
+
+const WeatherDataModel = mongoose.model('WeatherData', weatherDataSchema);
+
+module.exports = WeatherDataModel;
